4b5372aacf71d2d970dfa91fc7bf55aa8f8012d4
PGatak <pozycjonowanieadsum@gmail.com>
2019-08-27 22:08:15 +0200

    Modification of result display

--- statistics/tasks/01/nonparametric_tests.py
+++ statistics/tasks/01/nonparametric_tests.py
@@ -1,7 +1,7 @@
 from scipy import stats
 from preparation import data_1, data_2
 
-
+print("\n\t\t\t\tdata_1.PRZED_1, data_1.PO_1")
 print(sum(data_1.PRZED_1) / len(data_1.PRZED_1))
 print(sum(data_1.PO_1) / len(data_1.PO_1))
 test = stats.ttest_rel(data_1.PRZED_1, data_1.PO_1)
@@ -9,6 +9,7 @@ print(test)
 test = stats.wilcoxon(data_1.PRZED_1, data_1.PO_1)
 print(test)
 
+print("\n\t\t\t\tdata_2.PRZED_2, data_2.PO_2")
 print(sum(data_2.PRZED_2) / len(data_2.PRZED_2))
 print(sum(data_2.PO_2) / len(data_2.PO_2))
 test = stats.ttest_rel(data_2.PRZED_2, data_2.PO_2)
@@ -16,6 +17,7 @@ print(test)
 test = stats.wilcoxon(data_2.PRZED_2, data_2.PO_2)
 print(test)
 
+print("\n\t\t\t\tdata_1.PRZED_1, data_2.PRZED_2")
 print(sum(data_1.PRZED_1) / len(data_1.PRZED_1))
 print(sum(data_2.PRZED_2) / len(data_2.PRZED_2))
 test = stats.ttest_ind(data_1.PRZED_1, data_2.PRZED_2)
@@ -23,6 +25,7 @@ print(test)
 test = stats.mannwhitneyu(data_1.PRZED_1, data_2.PRZED_2)
 print(test)
 
+print("\n\t\t\t\tdata_1.PO_1, data_2.PO_2")
 print(sum(data_1.PO_1) / len(data_1.PO_1))
 print(sum(data_2.PO_2) / len(data_2.PO_2))
 test = stats.ttest_ind(data_1.PO_1, data_2.PO_2)
